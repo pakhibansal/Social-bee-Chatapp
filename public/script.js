f5762e2efcc1ee0768c351df719c15f11b98e84b
Pakhi Bansal <55701904+pakhibansal@users.noreply.github.com>
2020-03-31 13:51:32 +0530

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,38 @@
+
+const socket = io()
+
+setTimeout(()=>{
+    console.log(socket.id)
+},100)
+
+socket.on('chat_recieved',(data)=>{
+    console.log('Chat recieved successfully')
+    $('#list').append(
+        $('<li>').text(
+            `${data.username} : ${data.msg}`
+        )
+    )
+})
+
+socket.on('signup_successfull',()=>{
+    console.log('Signup successfully')
+    $('#signupbox').hide()
+    $('#chatbox').show()
+})
+
+$(()=>{
+    $('#chatbox').hide()
+    $('#signup').click(()=>{
+        $(".bg").css("background-image", "url('pic.png')");
+        socket.emit('signup',{
+            username:$('#username').val()  
+        })
+
+    })
+    $('#send').click(()=>{
+        console.log('Sending Chat')
+        socket.emit('chat',{
+            msg:$('#msg').val()
+        })
+    })
+})
